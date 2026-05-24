dd36deb9ffaa15c4700e5f24f01aada163aaadf9
ar-bebop <aribeir4@gmail.com>
2026-05-24 18:13:30 -0400

    fish updates

--- dot_config/fish/conf.d/10-vars.fish
+++ dot_config/fish/conf.d/10-vars.fish
@@ -1,9 +1,9 @@
 # pure
+set -U pure_symbol_prompt '$'
 set -U pure_enable_single_line_prompt true
 set -U fish_transient_prompt 1
-set -U pure_enable_single_line_prompt true
 set -U pure_show_prefix_root_prompt true
-set -U pure_symbol_prompt '$'
+set -U pure_symbol_container_prefix '📦 '
 
 # done
 set -U __done_min_cmd_duration 10000
