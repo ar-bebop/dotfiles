ca349445ec3f0187007e7cd7c35b93ae8054582e
ar-bebop <aribeir4@gmail.com>
2026-05-24 17:49:52 -0400

    fish updates

--- dot_config/fish/completions/box.fish
+++ dot_config/fish/completions/box.fish
@@ -1,10 +1,24 @@
+function __fish_box_needs_command
+    set -l cmd (commandline -opc)
+    test (count $cmd) -eq 1
+end
+
+function __fish_box_using_command
+    set -l cmd (commandline -opc)
+    test (count $cmd) -ge 2
+    and test "$cmd[2]" = "$argv[1]"
+end
+
 complete -c box -f
 
-complete -c box -n '__fish_use_subcommand' -a init   -d 'Initialize a box in current directory'
-complete -c box -n '__fish_use_subcommand' -a enter  -d 'Enter current directory box'
-complete -c box -n '__fish_use_subcommand' -a status -d 'Show current box info'
-complete -c box -n '__fish_use_subcommand' -a rm     -d 'Remove container, keep .box'
-complete -c box -n '__fish_use_subcommand' -a purge  -d 'Remove container and .box'
-complete -c box -n '__fish_use_subcommand' -a name   -d 'Print container name'
+complete -c box -n '__fish_box_needs_command' -a init -d 'Initialize box, provision packages, seed dotfiles'
+complete -c box -n '__fish_box_needs_command' -a enter -d 'Enter current directory box; auto-stop on exit'
+complete -c box -n '__fish_box_needs_command' -a status -d 'Show current box info'
+complete -c box -n '__fish_box_needs_command' -a provision -d 'Install or update baseline packages inside box'
+complete -c box -n '__fish_box_needs_command' -a seed -d 'Apply chezmoi dotfiles and fish plugins into ./home'
+complete -c box -n '__fish_box_needs_command' -a stop -d 'Stop current directory container'
+complete -c box -n '__fish_box_needs_command' -a rebuild -d 'Recreate container, keep ./home'
+complete -c box -n '__fish_box_needs_command' -a purge -d 'Remove container and delete .box/ and ./home'
+complete -c box -n '__fish_box_needs_command' -a name -d 'Print container name'
 
-complete -c box -n '__fish_seen_subcommand_from enter status rm purge name' -f
+complete -c box -n '__fish_box_using_command init' -a '(basename (pwd) | string replace -ra "[^A-Za-z0-9._-]" "-")' -d 'Default box name'
