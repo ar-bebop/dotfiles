bb57089b8f94d1dc2c02bd059c028e3b5254a079
ar-bebop <aribeir4@gmail.com>
2026-05-25 01:10:45 -0400

    scripts

--- dot_config/fish/completions/box.fish
+++ dot_config/fish/completions/box.fish
@@ -11,7 +11,7 @@ end
 
 complete -c box -f
 
-complete -c box -n '__fish_box_needs_command' -a init -d 'Initialize box, provision packages, seed dotfiles'
+complete -c box -n '__fish_box_needs_command' -a init -d 'Initialize box, install packages, seed dotfiles'
 complete -c box -n '__fish_box_needs_command' -a enter -d 'Enter current directory box; auto-stop on exit'
 complete -c box -n '__fish_box_needs_command' -a status -d 'Show current box info'
 complete -c box -n '__fish_box_needs_command' -a provision -d 'Install or update baseline packages inside box'
@@ -19,6 +19,8 @@ complete -c box -n '__fish_box_needs_command' -a seed -d 'Apply chezmoi dotfiles
 complete -c box -n '__fish_box_needs_command' -a stop -d 'Stop current directory container'
 complete -c box -n '__fish_box_needs_command' -a rebuild -d 'Recreate container, keep ./home'
 complete -c box -n '__fish_box_needs_command' -a purge -d 'Remove container and delete .box/ and ./home'
+complete -c box -n '__fish_box_needs_command' -a clean -d 'Remove orphan container for this directory'
+complete -c box -n '__fish_box_needs_command' -a doctor -d 'Check box dependencies and state'
 complete -c box -n '__fish_box_needs_command' -a name -d 'Print container name'
 
 complete -c box -n '__fish_box_using_command init' -a '(basename (pwd) | string replace -ra "[^A-Za-z0-9._-]" "-")' -d 'Default box name'
