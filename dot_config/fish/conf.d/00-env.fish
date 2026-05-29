ff88af00325cb3ef3f7c1ab4974e89ac279fd922
ar-bebop <aribeir4@gmail.com>
2026-05-29 03:14:04 -0400

    Update .config/fish/conf.d/00-env.fish Update .config/noctalia/settings.json

--- dot_config/fish/conf.d/00-env.fish
+++ dot_config/fish/conf.d/00-env.fish
@@ -1,5 +1,12 @@
 set -g fisher_path $HOME/.local/share/fisher
 
+set -g fish_cursor_default     line blink
+set -g fish_cursor_insert      line blink
+set -g fish_cursor_visual      block blink
+set -g fish_cursor_replace_one underscore blink
+set -g fish_cursor_replace     underscore blink
+set -g fish_cursor_external    line blink
+
 # Format man pages
 set -gx MANROFFOPT "-c"
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
