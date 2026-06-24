8dff5c4acabed958677e4d85627da8f7918554d6
ar-bebop <aribeir4@gmail.com>
2026-06-24 03:17:22 -0400

    Update .config/ghostty/config.ghostty Update .config/niri/cfg/layout.kdl Update .config/tmux/statusline.conf Update .config/tmux/tmux.conf Update .config/yazi/init.lua Update .config/yazi/theme.toml Update .config/yazi/yazi.toml

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,3 +1,41 @@
-pcall(function()
-    require("full-border"):setup { type = ui.Border.PLAIN }
-end)
+local function borders()
+	local old_build = Tab.build
+
+	Tab.build = function(self, ...)
+		local bar = function(c, x, y)
+			if x <= 0 or x == self._area.w - 1 or th.mgr.border_symbol ~= "│" then
+				return ui.Bar(ui.Edge.TOP)
+			end
+
+			return ui.Bar(ui.Edge.TOP)
+				:area(ui.Rect {
+					x = x,
+					y = math.max(0, y),
+					w = ya.clamp(0, self._area.w - x, 1),
+					h = math.min(1, self._area.h),
+				})
+				:symbol(c)
+		end
+
+		local c = self._chunks
+		self._chunks = {
+			c[1]:pad(ui.Pad.y(1)),
+			c[2]:pad(ui.Pad.y(1)),
+			c[3]:pad(ui.Pad.y(1)),
+		}
+
+		local style = th.mgr.border_style
+		self._base = ya.list_merge(self._base or {}, {
+			ui.Border(ui.Edge.ALL):area(self._area):type(ui.Border.PLAIN):style(style),
+
+			bar("┬", c[2].x, c[1].y),
+			bar("┴", c[2].x, c[1].bottom - 1),
+			bar("┬", c[2].right - 1, c[2].y),
+			bar("┴", c[2].right - 1, c[2].bottom - 1),
+		})
+
+		old_build(self, ...)
+	end
+end
+
+borders()
