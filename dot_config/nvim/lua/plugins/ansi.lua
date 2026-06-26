ecef6b762b586244b7735e22ba41679d75ffdcb2
ar-bebop <aribeir4@gmail.com>
2026-06-26 05:21:22 -0400

    Remove .config/nvim/lua/plugins/ansi.lua

--- dot_config/nvim/lua/plugins/ansi.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'stevedylandev/ansi-nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.cmd('colorscheme ansi')
-        vim.opt.termguicolors = false
-    end,
-}
